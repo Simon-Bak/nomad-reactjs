6f07f69d1456b9b620f76bc8d50df9ce7798ba55
Simon-Bak <bakdogyun@gmail.com>
2019-12-24 00:26:24 +0900

    #2.1 props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Test from './Test';
+
+function Test2(props){
+  return <h1>hihi {props.name2} </h1>
+}
 
 function App() {
   return (
     <div>HELLO!!
-    <Test />
-
-
-
+    <Test2 name='hola' name2 = 'hohoh'/>
 
     </div>
-
-    
   );
 }
 
--- src/Test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Test()
-{
-    return <h3>this is test</h3>;
-}
-
-export default Test;
